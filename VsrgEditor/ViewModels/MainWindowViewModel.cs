7942ef80a412911d16ae4a2e6e82a67f2970f969
이한석 <bluegkstjr@kaist.ac.kr>
2021-12-27 16:23:57 +0900

    Add some directories

---
+++ VsrgEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace VsrgEditor.ViewModels;
+
+public class MainWindowViewModel : ObservableRecipient
+{
+    public MainWindowViewModel()
+    {
+
+    }
+}
